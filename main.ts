a2cf7a46878348b4e63f0ed9c72435fd75179eaf
Robotsrule4545 <caleb.mincraft18@gmail.com>
2023-05-10 17:56:06 +0800

    Initial commit.

--- main.ts
+++ main.ts
@@ -1,19 +1,5 @@
+let _player = sprites.create(assets.image`player_gun1`, SpriteKind.Player)
 let current_weapon = 0
-let _player = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+controller.moveSprite(_player, 100, 100)
+_player.ay = 50
+_player.setStayInScreen(true)
