366a1dbf981f63231aa3a04c3ed3094b25083622
Robotsrule4545 <caleb.mincraft18@gmail.com>
2023-05-15 10:15:56 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,15 +9,18 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Platform, function (sprite, otherSprite) {
-    if (sprite.x > otherSprite.y) {
-        sprite.setPosition(otherSprite.x, otherSprite.y - 15)
+    if (sprite.x >= otherSprite.y - 2) {
+        sprite.setPosition(sprite.x, otherSprite.y - 15)
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Superplatform, function (sprite, otherSprite) {
     sprite.setPosition(otherSprite.x, otherSprite.y - 45)
+    sprites.destroy(otherSprite)
 })
 let currentPlatform: Sprite = null
 let platformRNG = 0
+let currentEnemy: Sprite = null
+let enemyRNG = 0
 let cooldown = 0
 let _player: Sprite = null
 _player = sprites.create(assets.image`player_gun1`, SpriteKind.Player)
@@ -28,6 +31,67 @@ _player.setStayInScreen(true)
 scene.setBackgroundColor(0)
 game.onUpdateInterval(2000, function () {
     cooldown = 0
+})
+game.onUpdateInterval(500, function () {
+    enemyRNG = randint(0, 100)
+    if (enemyRNG < 15) {
+        currentEnemy = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+    } else if (enemyRNG >= 40 && enemyRNG <= 100) {
+        currentEnemy = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+    } else if (enemyRNG >= 40 && enemyRNG <= 100) {
+        currentEnemy = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+    }
 })
 game.onUpdateInterval(300, function () {
     platformRNG = randint(0, 100)
